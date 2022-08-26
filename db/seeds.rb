13f802f9318d7f2e57bd584aa25961e14aac6981
krantosdev <krantosdev@gmail.com>
2022-08-26 18:11:42 -0300

    added seed data to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+todo_list = TodoList.new
+todo_list.title = 'Comprar en el super'
+todo_list.description = 'Ir al supermercado y comprar las cosas de la lista'
+todo_list.save
+
+todo_item = TodoItem.new
+todo_item.todo_list = todo_list
+todo_item.content = 'Shampoo'
+
+todo_item.save
+
